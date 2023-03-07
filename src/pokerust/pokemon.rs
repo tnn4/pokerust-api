cd8bdce75109a50da06a5fe0ad0f7d1cec6f9b47
tnn4 <tnn130130@gmail.com>
2023-03-06 20:06:18 -0600

    add serialization tests

--- src/pokerust/pokemon.rs
+++ src/pokerust/pokemon.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 use serde::{Serialize, Deserialize};
 
 use serde_json::Result;
@@ -9,17 +12,22 @@ use url::{Url, ParseError};
 struct Pokemon {
     name: String,
     national_pokedex_number: i16,
-    poketype: Vec<[PokeType; 2]>,
+    poketype: Vec<[Option<PokeType>; 2]>,
     stats: PokeStats,
     image_url: String,
+}
+
+impl Pokemon {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 enum PokeType {
     Fire,
-    Grass,
-    Water,
     Flying,
+    Grass,
+    Poison,
+    Water,
+    
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -38,7 +46,7 @@ pub fn create_json_by_serializing_data_structures() -> Result<()> {
     let charizard = Pokemon {
         name: "charizard".to_string(),
         national_pokedex_number: 6,
-        poketype: vec!([PokeType::Fire, PokeType::Flying]),
+        poketype: vec!([Some(PokeType::Fire), Some(PokeType::Flying)]),
         stats: PokeStats {
             hp: 78,
             atk: 84,
@@ -55,6 +63,55 @@ pub fn create_json_by_serializing_data_structures() -> Result<()> {
     // Print, write to file or send to HTTP server
 
     println!("{}", json_from_pokemon);
+
+    
+    Ok(())
+}
+
+pub fn serialize_to_toml() -> std::io::Result<()> {
+    // some data structure
+    let charizard = Pokemon {
+        name: "charizard".to_string(),
+        national_pokedex_number: 6,
+        poketype: vec!([Some(PokeType::Fire), Some(PokeType::Flying)]),
+        stats: PokeStats {
+            hp: 78,
+            atk: 84,
+            def: 78,
+            spatk: 109,
+            spdef: 85,
+            spd: 100,
+        },
+        image_url: "https://archives.bulbagarden.net/media/upload/thumb/3/38/0006Charizard.png/375px-0006Charizard.png".to_string(),
+    };
+
+    let toml = toml::to_string(&charizard).unwrap();
+
+    // https://serde.rs/convert-error.html
+    let mut file = File::create("charizard.toml")?;
+    file.write_all(toml.as_bytes());
+
+
+    let charizard = Pokemon {
+        name: "charizard".to_string(),
+        national_pokedex_number: 6,
+        poketype: vec!([Some(PokeType::Water), None]),
+        stats: PokeStats {
+            hp: 79,
+            atk: 83,
+            def: 100,
+            spatk: 85,
+            spdef: 105,
+            spd: 78,
+        },
+        image_url: "https://archives.bulbagarden.net/media/upload/thumb/0/0a/0009Blastoise.png/375px-0009Blastoise.png".to_string(),
+    };
+
+    let toml = toml::to_string(&charizard).unwrap();
+
+    // https://serde.rs/convert-error.html
+    let mut file = File::create("src/pokerust/blastoise.toml")?; // ? is shorthand for match Ok(T), Err from a Result
+    file.write_all(toml.as_bytes());
 
     Ok(())
 }
